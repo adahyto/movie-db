cd88cd79141d02c7fcda3c6d8c02a1dec7b6d799
adahyto <adahy.to@gmail.com>
2019-10-22 10:53:11 +0200

    app and service worker

---
+++ src/api/index.ts
@@ -0,0 +1,34 @@
+import MovieDB, { Configuration, Movie } from "moviedb-promise";
+import env from "../../aurelia_project/environments/dev";
+
+const client = new MovieDB(env.apiKey);
+
+let config: Configuration;
+
+client.configuration().then(c => (config = c));
+
+export class MovieApi {
+  search(query: string) {
+    return client.searchMovie({ query }).then(result =>
+      Object.assign(result, {
+        results: fixProfilePaths(result.results)
+      })
+    );
+  }
+
+  popularMovies() {
+    return client
+      .miscPopularMovies()
+      .then(result => fixProfilePaths(result.results));
+  }
+}
+
+const fixProfilePaths = (movies: Movie[]) => {
+  return movies
+    .filter(movie => movie.poster_path)
+    .map(movie => {
+      return Object.assign(movie, {
+        poster_path: `${config.images.secure_base_url}/w500${movie.poster_path}`
+      });
+    });
+};
